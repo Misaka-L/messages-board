d4e4668788485d3001e117deefb1fc4f78a118e5
Misaka-L <lipww1234@foxmail.com>
2024-05-24 20:18:12 +0800

    feature: localforage

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,67 @@
 <script setup lang="ts">
 import type { MessageWithId } from '~/types/message';
+import localforage from 'localforage'
 
-const { data: messages } = await useLazyFetch<MessageWithId[]>('/api/messages')
+// fetch data
+const { data: messages, error, status } = await useLazyFetch<MessageWithId[]>('/api/messages')
+
+// Localforage
+onMounted(async () => {
+  watch(status, async () => await updateLocalforage())
+
+  await updateLocalforage()
+})
+
+async function updateLocalforage() {
+  if (status.value === 'success') {
+    await saveDataToLocalforage()
+  }
+
+  if (status.value === 'error') {
+    await getDataFromLocalforage()
+  }
+}
+
+async function saveDataToLocalforage() {
+  await localforage.setItem('messages', JSON.stringify(messages.value))
+}
+
+async function getDataFromLocalforage() {
+  const data = await localforage.getItem<string>('messages')
+
+  if (!data) {
+    return
+  }
+
+  messages.value = await JSON.parse(data) as MessageWithId[]
+}
+
+// event source
+const isEventSourceOpened = ref(false)
+const isEventSourceErrored = ref(false)
+
+const eventSource = ref<EventSource | undefined>()
 
 onMounted(() => {
-  new EventSource('/api/messages/eventSource').addEventListener('message', (event) => {
+  eventSource.value = new EventSource('/api/messages/eventSource')
+
+  eventSource.value.addEventListener('message', async (event) => {
     messages.value = JSON.parse(event.data)
+    await saveDataToLocalforage()
+  })
+
+  eventSource.value.addEventListener('open', () => {
+    isEventSourceOpened.value = false
+    isEventSourceOpened.value = true
+  })
+
+  eventSource.value.addEventListener('error', () => {
+    isEventSourceOpened.value = false
+    isEventSourceErrored.value = true
   })
 })
 
+// highlight message
 const route = useRoute()
 const router = useRouter()
 
@@ -34,6 +87,15 @@ function tryHighlightMessage() {
     <n-card class="mb-4">
       <SubmitForm />
     </n-card>
+    <n-alert v-if="status === 'error' && !isEventSourceOpened" title="我们无法获取最新的留言消息" type="error" class="mb-4">
+      目前展示的留言为最后一次成功获取的留言消息。
+    </n-alert>
+    <n-alert v-if="isEventSourceErrored" title="与服务器的连接中断" type="error" class="mb-4">
+      留言将不会实时更新。
+    </n-alert>
+    <n-alert v-if="!isEventSourceOpened && !isEventSourceErrored" title="正在与服务器建立连接" type="info" class="mb-4">
+      建立连接后，留言将会实时更新。
+    </n-alert>
     <n-list hoverable>
       <n-list-item v-for="message in messages" class="border-emerald-600"
         :class="message.id === highlightMessageId ? 'border-[1px]' : ''">
